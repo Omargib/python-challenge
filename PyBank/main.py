62fe6d539414cf0aa5d583eb07e94f9108f68e3e
Omar Gibran Rivera Galvan <omargib86@hotmail.com>
2021-04-08 07:08:15 -0500

    update

---
+++ PyBank/main.py
@@ -0,0 +1,37 @@
+# import os module
+import os
+
+# improt module for reading csv file
+import csv
+
+# Lists to store data
+
+months = []
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+
+    for row in csvreader:
+        print(row)
+
+
+# --------------------------------------------------------------------------------------------------------------
+# WRITE THE FINAL OUTCOME
+
+# Set variable for output file
+output_file = os.path.join('Analysis', 'pybank_outcome.csv')
+
+#  Open the output file
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write the header row
+    writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
+                     "Percent of Reviews", "Length of Course"])
