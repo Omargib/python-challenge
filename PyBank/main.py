9a44e9d8dce0b15f2fbca135867b0619cae30fc8
Omar Gibran Rivera Galvan <omargib86@hotmail.com>
2021-04-09 00:49:26 -0500

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,12 @@ import csv
 # Lists to store data
 
 months = []
-
+total = []
+profit_losses = []
+idx = 0
+previews_total = 0
+max_month = 0
+min_month = 0
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
@@ -20,18 +25,51 @@ with open(csvpath) as csvfile:
 
     for row in csvreader:
         print(row)
+        months.append(row[0])
+        total.append(int(row[1]))
+        if idx == 0:
+            profit_losses.append(0)
+        else:
+            pl = int(row[1])-previews_total
+            profit_losses.append(pl)
+        idx = idx+1
+        previews_total = int(row[1])
 
+    print(len(months))
+    print(sum(total))
+    print(sum(profit_losses)/(idx-1))
+    print(max(profit_losses))
+    
+    max_month = profit_losses.index(max(profit_losses))
+    print(months[max_month])
+    min_month = profit_losses.index(min(profit_losses))
+    print(months[min_month])
+    print(min(profit_losses))
+
+
+
+
+    
 
 # --------------------------------------------------------------------------------------------------------------
 # WRITE THE FINAL OUTCOME
 
 # Set variable for output file
-output_file = os.path.join('Analysis', 'pybank_outcome.csv')
+output_file = os.path.join('Analysis', 'pybank_outcome.txt')
 
 #  Open the output file
 with open(output_file, "w", newline="") as datafile:
-    writer = csv.writer(datafile)
+    datafile.write(f"months:{len(months)}\n")
+
+    datafile.write(f"total:{sum(total)}\n")
+    datafile.write(f"average change {sum(profit_losses)/(idx-1)}\n")
+    
+    max_month = profit_losses.index(max(profit_losses))
+    datafile.write(f" Greatest Increase in Profits {months[max_month]} ${max(profit_losses)}\n")
+    min_month = profit_losses.index(min(profit_losses))
+    datafile.write(f" Greatest Decrease in Profits {months[min_month]} ${min(profit_losses)}\n")
+    
 
     # Write the header row
-    writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
-                     "Percent of Reviews", "Length of Course"])
+    # *writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
+                     # * "Percent of Reviews", "Length of Course"])
